124ee7062ecf9ce48d69d1118afb9419b82002f7
ifsheldon <maplevantablack@outlook.com>
2020-11-02 20:43:29 +0800

    added some code that may be useful

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod err;
+mod data;
+mod state;
+
+const OBJ_PATH: &'static str = "data/KAUST_Beacon.obj";
 fn main() {
-    println!("Hello, world!");
+    let (model, _) = tobj::load_obj(OBJ_PATH, true).expect("Loading Error");
+    println!("{}",model.len());
+    let mesh = &model.get(0).unwrap().mesh;
 }
